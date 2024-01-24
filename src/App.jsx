7dc3cba7d910c2753df81ce43c2d849220d60d66
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-01-24 03:10:07 -0500

    refactor: fix bugs in components

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <>Hello</>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="product" element={<Product />} />
           <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />} />
         </Routes>
       </BrowserRouter>
     </div>
