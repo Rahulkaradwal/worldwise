68c6ae59f49ec7be96d61ad62eb221387091444d
Rahul Karadwal <rahulkaradwal2000@gmail.com>
2024-01-24 03:30:39 -0500

    feat: fetch the city data

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,29 @@ import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch("http://localhost:9000/cities");
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error in loading the cities");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
@@ -15,8 +36,8 @@ function App() {
           <Route path="product" element={<Product />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<p>cities</p>} />
-            <Route path="cities" element={<p>cities</p>} />
+            <Route index element={<CityList />} />
+            <Route path="cities" element={<CityList />} />
             <Route path="countries" element={<p>countries</p>} />
             <Route path="form" element={<p>form</p>} />
           </Route>
